72034b76f2112f830b1876c98474ea587d46c29e
韩旭gogogo <js_bin@163.com>
2022-10-10 10:29:32 +0800

    feature: add antd

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// === Components === //
+import { Button } from "antd";
+
+// === Styles === //
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">home page</Button>
+    </div>
+  );
+}
+
+export default App;
