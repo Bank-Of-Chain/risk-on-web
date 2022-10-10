580bc6ba447d6bd93f6f7dd3f1c69e78f72c72a3
韩旭gogogo <js_bin@163.com>
2022-10-10 10:02:55 +0800

    chore: config update

--- config-overrides.js
+++ config-overrides.js
@@ -10,15 +10,14 @@ const FileManagerPlugin = require("filemanager-webpack-plugin");
 
 const resolve = (dir) => path.join(__dirname, ".", dir);
 
-const { NODE_ENV, ASSET_PATH } = process.env;
-console.log("ASSET_PATH=", ASSET_PATH);
+const { NODE_ENV } = process.env;
 module.exports = function override(config) {
-  config.output.publicPath = ASSET_PATH;
   config.resolve.alias = {
     ...config.resolve.alias,
     "@": resolve("src"),
   };
   if (NODE_ENV === "production") {
+    config.output.publicPath = "/risk-on-web/";
     config.plugins.push(
       new FileManagerPlugin({
         events: {
