1896ba74a2201c320f8a2f827800559eaaed057c
韩旭gogogo <js_bin@163.com>
2022-10-10 11:12:37 +0800

    refactor: remove unused codes

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
 
 // === Components === //
 import App from "./App";
