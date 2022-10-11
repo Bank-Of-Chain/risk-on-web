9531a8817940a1896d24c6130bfd7704e559f3eb
韩旭gogogo <js_bin@163.com>
2022-10-11 14:41:02 +0800

    feature: footer and header add

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// === Components === //
+import { Row, Col } from 'antd'
+
+const Footer = () => {
+  return (
+    <Row>
+      <Col span={24} style={{ textAlign: 'center' }}>
+        @2022 Bank of Chain, all rights reserved.
+      </Col>
+    </Row>
+  )
+}
+
+export default Footer
