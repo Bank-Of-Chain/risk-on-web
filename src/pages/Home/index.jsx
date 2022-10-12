d49030080cf3a7779ae4d1520420bd558c82da95
韩旭gogogo <js_bin@163.com>
2022-10-12 14:14:40 +0800

    feature: page add

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 // === Components === //
 import { Link } from 'react-router-dom'
-import { Button } from 'antd'
+import { Button, Row, Col, Typography, Card, List } from 'antd'
 
 const Home = () => {
+  const [personalVault, setPersonalVault] = useState([])
+
+  useEffect(() => {
+    setPersonalVault([
+      'Racing car sprays burning fuel into crowd.',
+      'Japanese princess to wed commoner.',
+      'Australian walks 100km after outback crash.',
+      'Man charged over missing wedding girl.',
+      'Los Angeles battles huge wildfires.'
+    ])
+  }, [])
+
   return (
-    <div>
-      <p>home</p>
-      <Button type="primary">
-        <Link to="/add">go to /add</Link>
-      </Button>
-      <Button type="primary">
-        <Link to="/analysis">go to /analysis</Link>
-      </Button>
-    </div>
+    <Row>
+      <Col span={12} push={6}>
+        <Card>
+          <Row>
+            <Col span={24}>
+              <List
+                header={<div>Personal Vaults</div>}
+                footer={
+                  <div>
+                    <Button type="primary">
+                      <Link to="/add">Add new Vault</Link>
+                    </Button>
+                  </div>
+                }
+                bordered
+                dataSource={personalVault}
+                renderItem={(item, index) => (
+                  <List.Item
+                    actions={[
+                      <Button type="link">
+                        <Link to={`/deposit/${index}`}>Deposit-{index}</Link>
+                      </Button>
+                    ]}
+                  >
+                    <Typography.Text mark>[USDC]</Typography.Text> {item}
+                  </List.Item>
+                )}
+              />
+            </Col>
+          </Row>
+        </Card>
+      </Col>
+    </Row>
   )
 }
 
